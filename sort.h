1ee2be784d19b409f717fdcf572e3229837599b3
Uwe Flumm <ucflumm@gmail.com>
2023-09-11 23:15:43 +1000

    added helper functions

--- sort.h
+++ sort.h
@@ -28,4 +28,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Extra functions used in quicksort */
+void quick_sort_recursion(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+
 #endif
